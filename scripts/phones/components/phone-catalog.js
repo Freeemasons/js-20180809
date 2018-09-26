d371126cba26f33f9ea99af69a142f31f61cbd22
Anastasia Pakkar <shivasmile11@gmail.com>
2018-09-27 02:53:10 +0300

    sort

--- scripts/phones/components/phone-catalog.js
+++ scripts/phones/components/phone-catalog.js
@@ -1,23 +1,27 @@
-import Component from  '../../common/component.js';
+import Component from "../../common/component.js";
 
 export default class PhoneCatalog extends Component {
-  constructor({element}) {
+  constructor({ element }) {
     super({ element });
 
-    this.on('click', '[data-element="phone-link"]', (event) => {
+    this.on("click", '[data-element="phone-link"]', event => {
       event.preventDefault();
       let phoneLink = event.delegateTarget;
-      let phoneElement = phoneLink.closest('[data-element="phone"]')
+      let phoneElement = phoneLink.closest('[data-element="phone"]');
 
-      this._trigger('phoneSelected', phoneElement.dataset.phoneId);
+      this._trigger("phoneSelected", phoneElement.dataset.phoneId);
     });
 
-    this.on('click', '[data-element="button-add"]', (event) => {
-      let phoneElement = event.delegateTarget.closest('[data-element="phone"]')
+    this.on("click", '[data-element="button-add"]', event => {
+      let phoneElement = event.delegateTarget.closest('[data-element="phone"]');
 
-      this._trigger('addToShoppingCart', phoneElement.dataset.phoneId);
-    })
+      this._trigger("addToShoppingCart", phoneElement.dataset.phoneId);
+    });
 
+    this._catalog.on("sortedArray", event => {
+      const sortedPhones = event._phones;
+      showPhones(sortedPhones);
+    });
   }
 
   showPhones(phones) {
@@ -26,14 +30,18 @@ export default class PhoneCatalog extends Component {
     this.show();
   }
 
-
   _render() {
     this._element.innerHTML = `
       <ul class="phones">
-        ${
-           this._phones.map(phone => `
-           <li data-phone-id="${phone.id}" data-element="phone" class="thumbnail">
-                    <a data-element="phone-link" href="#!/phones/${phone.id}" class="thumb">
+        ${this._phones
+          .map(
+            phone => `
+           <li data-phone-id="${
+             phone.id
+           }" data-element="phone" class="thumbnail">
+                    <a data-element="phone-link" href="#!/phones/${
+                      phone.id
+                    }" class="thumb">
                         <img alt="${phone.name}" src="${phone.imageUrl}">
                     </a>
 
@@ -43,11 +51,14 @@ export default class PhoneCatalog extends Component {
                         </a>
                     </div>
 
-                    <a data-element="phone-link" href="#!/phones/${phone.id}">${phone.name}</a>
+                    <a data-element="phone-link" href="#!/phones/${phone.id}">${
+              phone.name
+            }</a>
                     <p>${phone.snippet}</p>
                 </li>
-           `).join('') 
-        }
+           `
+          )
+          .join("")}
                 
                
             </ul>
--- scripts/phones/components/phone-filter.js
+++ scripts/phones/components/phone-filter.js
@@ -1,12 +1,47 @@
-import Component from '../../common/component.js';
+import Component from "../../common/component.js";
 
 export default class PhoneFilter extends Component {
-  constructor({element}) {
-    super({element});
+  constructor({ element }) {
+    super({ element });
     this._element = element;
+    console.log(element);
+    console.log(this);
 
     this._render();
+    this.filterSelectHandler();
+
+    this.on("click", "phoneFilter-alphabetical", event => {
+      console.log("------------------------------------");
+      console.log(event);
+      console.log("------------------------------------");
+      //   let phoneElement = event.delegateTarget.closest('[data-element="phone"]');
+
+      //   this._trigger("addToShoppingCart", phoneElement.dataset.phoneId);
+    });
   }
+
+  filterSelectHandler() {
+    let select = document.getElementById("phoneFilter-alphabetical");
+    console.log(select);
+    let newFilterState = select.value;
+    console.log(newFilterState);
+    return newFilterState;
+  }
+
+  _onFilterChange(optionValue) {
+    const items = this._items;
+    let sortedArray = [];
+    if (optionValue === "alphabetical") {
+      soretdArray = items.sort((a, b) => {
+        return a.name > b.name;
+      });
+    }
+    return soretdArray;
+  }
+
+  //остается передать в каталог
+  //отработала по аналогии this.catalog.onchange, то коллбек от нее отрисовал новый вариант и передать массив
+  //sortedArray
 
   _render() {
     this._element.innerHTML = `
@@ -17,11 +52,11 @@ export default class PhoneFilter extends Component {
 
 <p>
     Sort by:
-    <select>
-        <option value="name">Alphabetical</option>
+    <select id='phoneFilter-alphabetical' onchange='this.sortPhones()'>
+        <option data-element="phoneFilter-alphabetical" value="name">Alphabetical</option>
         <option value="age">Newest</option>
     </select> 
 </p>
-    `
+    `;
   }
 }
